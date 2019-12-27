419b39aea41fb8148f6b175f23ed31ec775f3cce
Curt Cox <curtcox@gmail.com>
2019-12-27 10:42:10 -0600

    Make clouds out of ovals.

--- analog_clock/lib/cloudy.dart
+++ analog_clock/lib/cloudy.dart
@@ -58,30 +58,33 @@ class CloudyPainter extends CustomPainter {
   }
 
   void _cloud1(double y, double speed) {
-      _circle(-0.05,  0.01 + y,  0.6,  gray(0xFF), speed);
-      _circle( 0.0,   0.02 + y,  0.75, gray(0xEE), speed);
-      _circle( 0.07,  0.06 + y,  0.5,  gray(0xF8), speed);
+      _oval(-0.05,  0.01 + y,  0.6,  gray(0xFF), speed);
+      _oval( 0.0,   0.02 + y,  0.75, gray(0xEE), speed);
+      _oval( 0.07,  0.06 + y,  0.5,  gray(0xF8), speed);
   }
 
   void _cloud2(double y, double speed) {
-    _circle(-0.04,  0.01 + y,  0.6,  gray(0xFF), speed);
-    _circle(-0.08,  0.0  + y,  0.6,  gray(0xEE), speed);
-    _circle( 0.09,  0.06 + y,  0.6,  gray(0xF8), speed);
-    _circle( 0.06, -0.06 + y,  0.7,  gray(0xFB), speed);
+    _oval(-0.04,  0.01 + y,  0.6,  gray(0xFF), speed);
+    _oval(-0.08,  0.0  + y,  0.6,  gray(0xEE), speed);
+    _oval( 0.09,  0.06 + y,  0.6,  gray(0xF8), speed);
+    _oval( 0.06, -0.06 + y,  0.7,  gray(0xFB), speed);
   }
 
   void _cloud3(double y, double speed) {
-    _circle(-0.04,  0.02 + y,  0.55,  gray(0xFF), speed);
-    _circle(-0.08, -0.0  + y,  0.65,  gray(0xEE), speed);
-    _circle( 0.08,  0.06 + y,  0.6,   gray(0xF8), speed);
-    _circle( 0.06, -0.04 + y,  0.44,  gray(0xFB), speed);
-    _circle(-0.06, -0.06 + y,  0.68,  gray(0xF4), speed);
+    _oval(-0.04,  0.02 + y,  0.55,  gray(0xFF), speed);
+    _oval(-0.08, -0.0  + y,  0.65,  gray(0xEE), speed);
+    _oval( 0.08,  0.06 + y,  0.6,   gray(0xF8), speed);
+    _oval( 0.06, -0.04 + y,  0.44,  gray(0xFB), speed);
+    _oval(-0.06, -0.06 + y,  0.68,  gray(0xF4), speed);
   }
 
   Color gray(int value) => Color.fromARGB(0xCC, value, value, value);
 
-  void _circle(double x, double y, double r, Color color, double speed) {
-    _canvas.drawCircle(_center(x,y,speed), _radius(r), _paint(color));
+  void _oval(double x, double y, double r, Color color, double speed) {
+    final width = _radius(r) * 2.22;
+    final height = width * 0.62;
+    final rect = Rect.fromCenter(center:_center(x,y,speed),width: width,height:height);
+    _canvas.drawOval(rect, _paint(color));
   }
 
   Paint _paint(Color color) => Paint()
