4e39d56f639fcbfa2c479730d8dea65cefb8ffc9
Curt Cox <curtcox@gmail.com>
2019-12-27 10:26:40 -0600

    Delete unused params. More fog.

--- analog_clock/lib/foggy.dart
+++ analog_clock/lib/foggy.dart
@@ -44,26 +44,27 @@ class FoggyPainter extends CustomPainter {
   }
 
   void _paintFog() {
-      _circle(0.0,0.5,2.5,gray(230),42,0.0);
-      _circle(0.0,0.5,2.5,gray(240),42,0.25);
-      _circle(0.0,0.5,2.5,gray(250),42,0.5);
-      _circle(0.0,0.5,2.5,gray(255),42,0.75);
-      _circle(0.0,0.5,2.6,gray(245),87,0.2);
-      _circle(0.0,0.5,2.6,gray(220),87,0.4);
-      _circle(0.0,0.5,2.6,gray(225),87,0.6);
+      _circle(2.50,gray(230),42,0.0);
+      _circle(2.50,gray(240),42,0.25);
+      _circle(2.50,gray(250),42,0.5);
+      _circle(2.50,gray(255),42,0.75);
+      _circle(2.60,gray(245),87,0.2);
+      _circle(2.60,gray(220),87,0.4);
+      _circle(2.60,gray(225),87,0.6);
+      _circle(2.55,gray(237),870,0.6);
   }
 
   Color gray(int value) => Color.fromARGB(80, value, value, value);
 
-  void _circle(double x, double y, double r, Color color, double speed,double offset) {
-    _canvas.drawCircle(_center(x,y,speed,offset), _radius(r), _paint(color));
+  void _circle(double r, Color color, double speed,double offset) {
+    _canvas.drawCircle(_center(speed,offset), _radius(r), _paint(color));
   }
 
   Paint _paint(Color color) => Paint()
     ..color = color;
 
-  Offset _center(double x, double y, double speed, double offset)
-    => Offset((_x(speed,offset) + x) * _size.width,  y * _size.height);
+  Offset _center(double speed, double offset)
+    => Offset(_x(speed,offset) * _size.width, 0.5 * _size.height );
 
   double _radius(double r) => (r * _size.height) / 5;
 
