4b1e245af9bdbdc53f4922d97224ab66dea1aadc
Curt Cox <curtcox@gmail.com>
2019-12-31 14:32:15 -0600

    Better snow.

--- analog_clock/lib/snowy.dart
+++ analog_clock/lib/snowy.dart
@@ -20,64 +20,44 @@ class SnowyPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-      _canvas = canvas;
-      _size = size;
-      _paintClouds();
+    _canvas = canvas;
+    _size = size;
+    _paintRain();
   }
 
-  void _paintClouds() {
-      _cloud1(0.0,101);
-      _cloud2(0.1,203);
-      _cloud3(0.2,307);
-      _cloud1(0.3,151);
-      _cloud2(0.4,252);
-      _cloud3(0.5,357);
-      _cloud1(0.6,057);
-      _cloud2(0.7,177);
-      _cloud3(0.8,277);
-      _cloud1(0.9,127);
-      _cloud2(1.0,147);
+  void _paintRain() {
+    double max = 501;
+    for (int i=1; i<max; i++) {
+      double distance = i / max;
+      double x = (i + i * i) % max / max;
+      _drop(x,10 + distance * 40);
+    }
   }
 
-  void _cloud1(double y, double speed) {
-      _oval(-0.05,  0.01 + y,  0.6,  gray(0xFF), speed);
-      _oval( 0.0,   0.02 + y,  0.75, gray(0xEE), speed);
-      _oval( 0.07,  0.06 + y,  0.5,  gray(0xF8), speed);
+  void _drop(double x, double distance) {
+    double size = 10 / distance;
+    double speed = 80000 / distance;
+    _oval(x, size, gray(0xFF), speed);
   }
 
-  void _cloud2(double y, double speed) {
-    _oval(-0.04,  0.01 + y,  0.6,  gray(0xFF), speed);
-    _oval(-0.08,  0.0  + y,  0.6,  gray(0xEE), speed);
-    _oval( 0.09,  0.06 + y,  0.6,  gray(0xF8), speed);
-    _oval( 0.06, -0.06 + y,  0.7,  gray(0xFB), speed);
-  }
+  Color gray(int value) => Color.fromARGB(0xFF, value, value, value);
 
-  void _cloud3(double y, double speed) {
-    _oval(-0.04,  0.02 + y,  0.55,  gray(0xFF), speed);
-    _oval(-0.08, -0.0  + y,  0.65,  gray(0xEE), speed);
-    _oval( 0.08,  0.06 + y,  0.6,   gray(0xF8), speed);
-    _oval( 0.06, -0.04 + y,  0.44,  gray(0xFB), speed);
-    _oval(-0.06, -0.06 + y,  0.68,  gray(0xF4), speed);
-  }
-
-  Color gray(int value) => Color.fromARGB(0xCC, value, value, value);
-
-  void _oval(double x, double y, double r, Color color, double speed) {
-    final width = _radius(r) * 2.22;
-    final height = width * 0.62;
-    final rect = Rect.fromCenter(center:_center(x,y,speed),width: width,height:height);
+  void _oval(double x, double r, Color color, double speed) {
+    final width = _radius(r) * 0.11;
+    final height = width * 2.02;
+    final rect = Rect.fromCenter(center:_center(x,speed),width: width,height:height);
     _canvas.drawOval(rect, _paint(color));
   }
 
   Paint _paint(Color color) => Paint()
     ..color = color;
 
-  Offset _center(double x, double y, double speed)
-    => Offset((_x(speed) + x) * _size.width,  y * _size.height);
+  Offset _center(double x, double speed)
+  => Offset(x * _size.width, (1.0 - _y(speed)) * _size.height);
 
   double _radius(double r) => (r * _size.height) / 5;
 
-  double _x(double speed) => TimeCycle.at(time,speed,0.0,1.4,0.2);
+  double _y(double speed) => TimeCycle.at(time,speed,0.0,1.1,0.05);
 
   @override
   bool shouldRepaint(SnowyPainter old) => true;
