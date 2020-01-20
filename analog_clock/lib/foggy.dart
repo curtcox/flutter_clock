710f7430e232c08b3d142882cf115fd18190c7d1
Curt Cox <curt.cox@asynchrony.com>
2020-01-19 23:13:25 -0600

    Better fog.

--- analog_clock/lib/foggy.dart
+++ analog_clock/lib/foggy.dart
@@ -23,17 +23,12 @@ class FoggyPainter extends TimedCustomPainter {
   }
 
   void _paintFog() {
-      _circle(2.50,gray(230),42,0.0);
-      _circle(2.50,gray(240),42,0.25);
-      _circle(2.50,gray(250),42,0.5);
-      _circle(2.50,gray(255),42,0.75);
-      _circle(2.60,gray(245),87,0.2);
-      _circle(2.60,gray(220),87,0.4);
-      _circle(2.60,gray(225),87,0.6);
-      _circle(2.55,gray(237),870,0.6);
+      for (int i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) {
+         _circle(2.7 + i / 71, gray(140 + i), i.toDouble(), i / 120);
+      }
   }
 
-  Color gray(int value) => Color.fromARGB(80, value, value, value);
+  Color gray(int value) => Color.fromARGB(40, value, value, value);
 
   void _circle(double r, Color color, double speed,double offset) {
     _canvas.drawCircle(_center(speed,offset), _radius(r), _paint(color));
@@ -43,7 +38,7 @@ class FoggyPainter extends TimedCustomPainter {
     ..color = color;
 
   Offset _center(double speed, double offset)
-    => Offset(_x(speed,offset) * _size.width, 0.5 * _size.height );
+    => Offset(_x(speed,offset) * _size.width, 0.5 * _size.height + -191 * offset);
 
   double _radius(double r) => (r * _size.height) / 5;
 
